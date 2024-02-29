d46a8cfee79b3034eb1a78327b2955edde5d9be8
Ahmed Ramadan El_Malky <Ahmed.RamadanCS21@fcai.usc.edu.eg>
2024-02-29 17:35:57 +0200

    fix UI responsiveness and finish tts functionality

--- app/src/main/java/com/elmalky/chathub/UI/MainActivity.kt
+++ app/src/main/java/com/elmalky/chathub/UI/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.elmalky.chathub.UI
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -8,13 +10,16 @@ import com.elmalky.chathub.Adapters.ChatRecyclerView
 import com.elmalky.chathub.MainViewModel
 import com.elmalky.chathub.R
 import com.elmalky.chathub.databinding.ActivityMainBinding
+import java.util.Locale
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     lateinit var binder: ActivityMainBinding
+    lateinit var tts: TextToSpeech
     val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binder = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        tts = TextToSpeech(this, this)
         binder.vm = viewModel
         binder.lifecycleOwner = this
         val adapter = ChatRecyclerView(mutableListOf(), viewModel)
@@ -29,6 +34,23 @@ class MainActivity : AppCompatActivity() {
 //            Log.i("Tag",it.toString())
 //        }
 
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS)
+            tts.language = Locale.UK
+    }
+
+    fun speakOut(view: View) {
+//        for (voice:Voice in tts.voices){
+//                if(voice.name == "en-gb-x-rjs#male_2-local") {
+//                    tts.voice = voice
+//                    break
+//                }
+//        }
+        viewModel.botChat.observe(this) {
+            tts.speak(it, TextToSpeech.QUEUE_FLUSH, null)
+        }
     }
 
 //    private fun setUpDrawer() {
